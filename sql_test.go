e7207f90c30a866c1eacf28855a2c31da161b198
regi <witantoregi@gmail.com>
2025-09-26 09:30:01 +0700

    Add TestAutoIncrement for inserting comments with auto-generated IDs

--- sql_test.go
+++ sql_test.go
@@ -172,3 +172,25 @@ func TestExecSqlParameter(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	email := "arman@gmail.com"
+	comment := "test komen"
+
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	result, err := db.ExecContext(ctx, script, email, comment)
+	if err != nil {
+		panic(err)
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new comment with id", insertId)
+}
